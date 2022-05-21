36ad67afa6c9f207ce1e98dfffa108ffe84201b0
ravindaa <ravindaa@oregonstate.edu>
2022-05-21 12:57:35 +0600

    Second commit

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
+# pip install matplotlib
 import matplotlib.pyplot as plt
+# pip install numpy
 import numpy as np
+# pip instal pandas
 import pandas as pd
 
-
-# 2D Data Structure assosiated with Python
 df = pd.read_excel('Test.xlsx')
 
-data_array = df.to_numpy().transpose()
-print(data_array)
+data_array = df.to_numpy()
+transposed_array = data_array.transpose()
+print(transposed_array)
 
-x = np.array(data_array[0])
-y = np.array(data_array[1])
+x = np.array(transposed_array[1])
+y = np.array(transposed_array[0])
+sorted_x = np.sort(-x)
+sorted_y = np.sort(-y)
+print(x)
+print(y)
 
-plt.plot(x, y)
+plt.plot(sorted_x, sorted_y, color='green', marker='o')
 plt.show()
